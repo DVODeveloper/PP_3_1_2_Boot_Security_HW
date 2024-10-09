85642f35af6261a128c27aa1a0c7f32453a25d7b
DVODeveloper <vyurkovd@yandex.ru>
2024-10-09 09:09:20 +0300

    registration process

---
+++ src/main/java/ru/kata/spring/boot_security/demo/util/PersonValidator.java
@@ -0,0 +1,38 @@
+package ru.kata.spring.boot_security.demo.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.kata.spring.boot_security.demo.models.Person;
+import ru.kata.spring.boot_security.demo.services.PersonDetailsService;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDetailsService personDetailsService;
+
+    @Autowired
+    public PersonValidator(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+
+        try {
+            personDetailsService.loadUserByUsername(person.getUsername());
+        } catch (UsernameNotFoundException ignored) {
+            return; // все ок, пользователь с таким именем не наден
+        }
+        errors.rejectValue("username", "", "Пользователь с таким именем уже существует");
+    }
+}
