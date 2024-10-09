b92196fb825df7a0ff8ab4cd8a5317ec3499b177
DVODeveloper <vyurkovd@yandex.ru>
2024-10-09 10:14:46 +0300

    logout

--- src/main/java/ru/kata/spring/boot_security/demo/configs/SecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/SecurityConfig.java
@@ -35,7 +35,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/auth/login")
                 .loginProcessingUrl("/process_login")
                 .defaultSuccessUrl("/hello", true)
-                .failureUrl("/auth/login?error=true");
+                .failureUrl("/auth/login?error=true")
+                .and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/auth/login");
     }
 
 
