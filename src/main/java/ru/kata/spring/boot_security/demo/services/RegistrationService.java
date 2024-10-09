20a20068d412e1e2649544e56a39d65ae3f0a19d
DVODeveloper <vyurkovd@yandex.ru>
2024-10-09 10:44:54 +0300

    encoder

--- src/main/java/ru/kata/spring/boot_security/demo/services/RegistrationService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RegistrationService.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.models.Person;
@@ -8,15 +9,19 @@ import ru.kata.spring.boot_security.demo.repositories.PeopleRepository;
 
 @Service
 public class RegistrationService {
+
+    private final PasswordEncoder passwordEncoder;
     private final PeopleRepository peopleRepository;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PasswordEncoder passwordEncoder, PeopleRepository peopleRepository) {
+        this.passwordEncoder = passwordEncoder;
         this.peopleRepository = peopleRepository;
     }
 
     @Transactional
     public void register(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         peopleRepository.save(person);
     }
 }
