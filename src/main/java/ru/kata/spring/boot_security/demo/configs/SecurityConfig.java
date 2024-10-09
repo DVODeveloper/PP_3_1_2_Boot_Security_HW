46992328f40185f5d71477f1236496e8f1e3b942
DVODeveloper <vyurkovd@yandex.ru>
2024-10-09 12:00:36 +0300

    Annotation @PreAuthorize

--- src/main/java/ru/kata/spring/boot_security/demo/configs/SecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +15,7 @@ import ru.kata.spring.boot_security.demo.services.PersonDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PersonDetailsService personDetailsService;
@@ -29,7 +31,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // конфигурируем авторизацию
         http
                 .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().hasAnyRole("ADMIN", "USER")
                 .and()
--- src/main/java/ru/kata/spring/boot_security/demo/controllers/HelloController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/HelloController.java
@@ -1,13 +1,23 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.kata.spring.boot_security.demo.security.PersonDetails;
+import ru.kata.spring.boot_security.demo.services.AdminService;
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
 
     @GetMapping("/hello")
     public String sayHello() {
@@ -24,6 +34,7 @@ public class HelloController {
 
     @GetMapping("/admin")
     public String adminPage() {
+        adminService.doAdminStuff();
         return "admin";
     }
 }
